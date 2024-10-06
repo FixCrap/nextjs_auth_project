4ef6398d66b68fc24ddea40336b1952313ec97d5
FixCrap <George_andriopoulos@hotmail.gr>
2024-10-06 16:40:12 +0300

    form+actions+zod validations

---
+++ app/register/page.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import {
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Form } from "@/components/ui/form";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { passwordMatchSchema } from "@/validation/passwordMatchSchema";
+import { registerUser } from "./action";
+
+const formSchema = z
+	.object({
+		email: z.string().email(),
+	})
+	.and(passwordMatchSchema);
+
+function Register() {
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+			passwordConfirm: "",
+		},
+	});
+
+	const handleSubmit = async (data: z.infer<typeof formSchema>) => {
+		const response = await registerUser({
+			email: data.email,
+			password: data.password,
+			passwordConfirm: data.passwordConfirm,
+		});
+		console.log(response);
+	};
+
+	return (
+		<main className='flex justify-center items-center min-h-screen '>
+			<Card className='w-[350px] '>
+				<CardHeader>
+					<CardTitle>Register</CardTitle>
+					<CardDescription>Register for a new account</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<Form {...form}>
+						<form
+							className='flex flex-col gap-2'
+							onSubmit={form.handleSubmit(handleSubmit)}>
+							<FormField
+								control={form.control}
+								name='email'
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Email</FormLabel>
+										<FormControl>
+											<Input
+												{...field}
+												type='email'
+												placeholder=''
+											/>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name='password'
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Password</FormLabel>
+										<FormControl>
+											<Input
+												{...field}
+												type='password'
+												placeholder=''
+											/>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name='passwordConfirm'
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Password confirm</FormLabel>
+										<FormControl>
+											<Input
+												{...field}
+												type='password'
+												placeholder=''
+											/>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<Button type='submit'>Register</Button>
+						</form>
+					</Form>
+				</CardContent>
+			</Card>
+		</main>
+	);
+}
+
+export default Register;
